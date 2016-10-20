062b1c5245af906b6de8aa903cd53c712c05e310
Luis Muñoz Gonzalez <LuisMunoz@la-MacBook-Pro-de-Luis.local>
2016-10-19 21:04:56 -0300

    Error form

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get 'welcome/index'
   root :to => 'welcome#index'
 
+  match ':controller(/:action(/:id))(.:format)'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
